7d5b30a7999eb6245b2eac966772b4e1217f8c07
irsyadulibad <ahmadirsyadulibad@gmail.com>
2021-01-30 15:35:38 +0700

    Add file's view to dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $files = DataFile::get();
+        return view('home', compact('files'));
     }
 }
